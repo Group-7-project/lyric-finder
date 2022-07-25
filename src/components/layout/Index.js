8e3972735d6c8d19e98add5b4872ba7788e9b479
David Sklar <davidhsklar@gmail.com>
2022-07-24 21:47:50 -0400

    frontend functionality done working on design and pwa

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
 import Tracks from '../tracks/Tracks'
 import Search from '../tracks/Search'
+
 export default class Index extends Component {
   render() {
     return (
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,36 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-/*import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';  */
+import React, { Component } from 'react'
 
-import Auth from '../utils/auth';
-
-
-
-  const Navbar = () => {
-    // set modal display state
-    const [showModal, setShowModal] = useState(false);
-  
+export default class Navbar extends Component {
+  render() {
     return (
-    
-        <Navbar bg='dark' variant='dark' expand='lg'>
-          <Container fluid>
-            <Navbar.Brand as={Link} to='/'>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='navbar' />
-            <Navbar.Collapse id='navbar'>
-              <Nav className='ml-auto'>
-                <Nav.Link as={Link} to='/'>
-                  Search For Lyrics
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        )}
-
-
-
-
-
+      <div>
+        <nav className="navbar navbar-dark bg-primary mb-5">
+            <span className="navbar-brand mb-0 h1 mx-auto">
+               Songs & lyrics
+            </span>
+            </nav>
+      </div>
+    )
+  }
+}
