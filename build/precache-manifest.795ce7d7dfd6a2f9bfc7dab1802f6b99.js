40aee618d7dc21c1556112a58c48adfb63ad949b
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-29 10:29:44 -0400

    updates

---
+++ build/precache-manifest.795ce7d7dfd6a2f9bfc7dab1802f6b99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ba05efb200a8dd8b09",
+    "url": "/static/js/main.c2ddf098.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbf82390277438b0bc1f",
+    "url": "/static/js/2.943f4a91.chunk.js"
+  },
+  {
+    "revision": "41ecc0fbe189e596deca5ad7ec67c308",
+    "url": "/index.html"
+  }
+];
